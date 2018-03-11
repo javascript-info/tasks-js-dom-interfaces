fd58af54f64203a1f619c4a78309b82263d70c00
Burlak Ilia <burlakilia@bk.ru>
2018-03-11 12:16:31 +0300

    Провекра вложенности

---
+++ task-4/task.test.js
@@ -0,0 +1,15 @@
+"use strict";
+
+"new request 5";
+
+describe("task-1 suite", function() {
+
+    it("при возведении 2 в 3ю степень результат 8", function() {
+        expect(pow(2, 3)).toEqual(8);
+    });
+
+    it("при возведении 3 в 4ю степень равен 81", function() {
+        expect(pow(3, 4)).toEqual(81);
+    });
+
+});
