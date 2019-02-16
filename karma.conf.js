8b1161f52bcd3bb2e02275833665f9508c6da644
Sergey Zelenov <mail@zelenov.su>
2019-02-16 10:28:44 +0100

    update karma.conf and travis.yml

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,6 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: './',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
@@ -35,25 +34,27 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'json'],
+    reporters: ['json'], // progress
 
     jsonReporter: {
       stdout: true,
-      outputFile: 'results/results.json' // defaults to none
+      // outputFile: 'results/results.json' // defaults to none
     },
 
 
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
+    colors: false,
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DISABLE,
+
+    browserConsoleLogOptions: {
+      terminal: false
+    },
 
 
     // enable / disable watching file and executing tests whenever any file changes
@@ -74,4 +75,3 @@ module.exports = function(config) {
     concurrency: Infinity
   })
 }
-
